a931e1311b1b486f80291a46134721e58e7d495d
HanHelmawan <raihanazz935@gmail.com>
2025-12-16 09:04:31 +0700

    233040135 : membuat routes dari tiap controller

--- padel-court-booking/routes/web.php
+++ padel-court-booking/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourtCategoriesController;
+use App\Http\Controllers\CourtsController;
+use App\Http\Controllers\BookingsController;
+use App\Http\Controllers\PaymentsController;
+use App\Http\Controllers\UserController;
 
+// Halaman Depan (Landing Page)
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+// Route Resource CRUD untuk masing-masing controller
+Route::resource('users', UserController::class);
+Route::resource('court-categories', CourtCategoriesController::class);
+Route::resource('courts', CourtsController::class);
+Route::resource('bookings', BookingsController::class);
+Route::resource('payments', PaymentsController::class);
